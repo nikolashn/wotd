176a904a0d9f4af71338d9f75a16333aa6091749
Nikolas Haarhoff-Nargi <nehn.2019@gmail.com>
2023-09-24 03:30:34 +0100

    Rename

--- wotd.hs
+++ wotd.hs
@@ -43,10 +43,10 @@ type Parser = Maybe (String, String)
 
 runParser :: String -> (String, String)
 runParser s = (word, def)
-  where (rest1, word) = fromMaybe syntaxError $ parseStr s
-        (rest2, _)    = fromMaybe syntaxError $ parseDropCh ':' rest1
-        (rest3, def)  = fromMaybe syntaxError $ parseStr rest2
-        syntaxError   = error "Invalid syntax in dictionary"
+  where (rest1, word) = fromJust' $ parseStr s
+        (rest2, _)    = fromJust' $ parseDropCh ':' rest1
+        (rest3, def)  = fromJust' $ parseStr rest2
+        fromJust'     = fromMaybe $ error "Invalid syntax in dictionary"
 
 parseStr :: String -> Parser
 parseStr s = do
